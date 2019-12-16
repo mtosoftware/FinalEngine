aca30da6b3ec8ea7f1237992de34b575b0eac5f2
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-16 20:21:52 +1100

    Create RasterStateDescription

---
+++ FinalEngine.Rendering/RasterStateDescription.cs
@@ -0,0 +1,51 @@
+﻿namespace FinalEngine.Rendering
+{
+    public enum CullFaceMode
+    {
+        Front,
+
+        Back
+    }
+
+    public enum RasterMode
+    {
+        Fill,
+
+        Line
+    }
+
+    public enum WindingDirection
+    {
+        Clockwise,
+
+        CounterClockwise
+    }
+
+    public struct RasterStateDescription
+    {
+        public static RasterStateDescription Default
+        {
+            get
+            {
+                return new RasterStateDescription()
+                {
+                    CullEnabled = false,
+                    CullFaceMode = CullFaceMode.Back,
+                    WindingDirection = WindingDirection.CounterClockwise,
+                    FillMode = RasterMode.Fill,
+                    ScissorEnabled = false
+                };
+            }
+        }
+
+        public bool CullEnabled { get; set; }
+
+        public CullFaceMode CullFaceMode { get; set; }
+
+        public RasterMode FillMode { get; set; }
+
+        public bool ScissorEnabled { get; set; }
+
+        public WindingDirection WindingDirection { get; set; }
+    }
+}
