02c8f5cfe7cbffc055eb7af719d6bd181ddde73d
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-16 20:22:51 +1100

    Create IRasterizer

---
+++ FinalEngine.Rendering/IRasterizer.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalEngine.Rendering
+{
+    public interface IRasterizer
+    {
+        void SetRasterState(RasterStateDescription description);
+
+        void SetScissor(int x, int y, int width, int height);
+
+        void SetViewport(int x, int y, int width, int height);
+    }
+}
