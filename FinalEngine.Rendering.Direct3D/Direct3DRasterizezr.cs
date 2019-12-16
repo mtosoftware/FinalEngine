3fa27d60736428abeccca14997418e4810f81a5c
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-16 20:34:44 +1100

    Starting on Direct3DRasterizer

---
+++ FinalEngine.Rendering.Direct3D/Direct3DRasterizezr.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalEngine.Rendering.Direct3D
+{
+    public sealed class Direct3DRasterizezr : IRasterizer
+    {
+        public void SetRasterState(RasterStateDescription description)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetScissor(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetViewport(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
