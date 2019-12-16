69f672efff55b93125e3bb8d93c2b4b34b886e69
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-16 20:26:13 +1100

    Starting on OpenGL rasterizer

---
+++ FinalEngine.Rendering.OpenGL/IOpenGLInvoker.cs
@@ -0,0 +1,6 @@
+﻿namespace FinalEngine.Rendering.OpenGL
+{
+    public interface IOpenGLInvoker
+    {
+    }
+}
---
+++ FinalEngine.Rendering.OpenGL/OpenGLRasterizer.cs
@@ -0,0 +1,29 @@
+﻿namespace FinalEngine.Rendering.OpenGL
+{
+    using System;
+
+    public sealed class OpenGLRasterizer : IRasterizer
+    {
+        private readonly IOpenGLInvoker invoker;
+
+        public OpenGLRasterizer(IOpenGLInvoker invoker)
+        {
+            this.invoker = invoker ?? throw new ArgumentNullException(nameof(invoker), $"The specified { nameof(invoker) } parameter is null.");
+        }
+
+        public void SetRasterState(RasterStateDescription description)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetScissor(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetViewport(int x, int y, int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
