2e3ac6a96325017fa1fad8c3d60796847f64fcc3
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 00:30:53 +1100

    Starting on resource factory

---
+++ FinalEngine.Rendering/IGPUResourceFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalEngine.Rendering
+{
+    using FinalEngine.Rendering.Buffers;
+
+    public interface IGPUResourceFactory
+    {
+        IBuffer CreateBuffer<T>(BufferType type, T[] data, int sizeInBytes, int structStride) where T : struct;
+    }
+}
