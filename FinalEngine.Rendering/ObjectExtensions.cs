3e90d258daf14622df16d44f16adebf4f80b1292
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 01:26:53 +1100

    Update documentation

--- FinalEngine.Rendering/ObjectExtensions.cs
+++ FinalEngine.Rendering/ObjectExtensions.cs
@@ -2,8 +2,29 @@
 {
     using System;
 
+    /// <summary>
+    ///   Provides extension methods for an <see cref="object"/>.
+    /// </summary>
     public static class ObjectExtensions
     {
+        /// <summary>
+        ///   Converts the specified <paramref name="obj"/> parameter to an object of the specified <typeparamref name="T"/>, type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   Specifies the type to convert the specified <paramref name="obj"/> too.
+        /// </typeparam>
+        /// <param name="obj">
+        ///   Specifies a <see cref="object"/> that represents the object to convert to the specified <typeparamref name="T"/>.
+        /// </param>
+        /// <returns>
+        ///   The specified <paramref name="obj"/>, converted to a <typeparamref name="T"/>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        ///   The specified <paramref name="obj"/> parameter is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        ///   The specified <paramref name="obj"/> is not of type <typeparamref name="T"/>.
+        /// </exception>
         public static T Cast<T>(this object obj)
         {
             if (obj == null)
--- FinalEngine.Rendering/PrimitiveTopology.cs
+++ FinalEngine.Rendering/PrimitiveTopology.cs
@@ -1,13 +1,28 @@
 ﻿namespace FinalEngine.Rendering
 {
+    /// <summary>
+    ///   Enumerates the available topologies that determine how consecutive vertices are organized into primitives, along with the type of primitive that will be used.
+    /// </summary>
     public enum PrimitiveTopology
     {
+        /// <summary>
+        ///   Specifies a list of isolated triangles.
+        /// </summary>
         Triangle,
 
+        /// <summary>
+        ///   Specifies a series of connected triangles.
+        /// </summary>
         TriangleStrip,
 
+        /// <summary>
+        ///   Specifies a list of isolated, straight line segments.
+        /// </summary>
         Line,
 
+        /// <summary>
+        ///   Specifies a series of connected line segments.
+        /// </summary>
         LineStrip
     }
 }
