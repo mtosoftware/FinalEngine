d35168214f187f9f4bf82ce9819d4f9ede904105
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 00:53:53 +1100

    More documentation

--- FinalEngine.Rendering/Buffers/IBuffer.cs
+++ FinalEngine.Rendering/Buffers/IBuffer.cs
@@ -2,10 +2,26 @@
 {
     using System;
 
+    /// <summary>
+    ///   Defines an interface that represents a primitive buffer resource.
+    /// </summary>
+    /// <seealso cref="System.IDisposable"/>
     public interface IBuffer : IDisposable
     {
+        /// <summary>
+        ///   Gets a <see cref="int"/> that represents the size in bytes of this <see cref="IBuffer"/>.
+        /// </summary>
+        /// <value>
+        ///   The size in bytes of this <see cref="IBuffer"/>.
+        /// </value>
         int SizeInBytes { get; }
 
+        /// <summary>
+        ///   Gets a <see cref="BufferType"/> that represents the type of this <see cref="IBuffer"/>.
+        /// </summary>
+        /// <value>
+        ///   The type of this <see cref="IBuffer"/>.
+        /// </value>
         BufferType Type { get; }
     }
 }
