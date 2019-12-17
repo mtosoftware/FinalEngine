c294770c088a514fb7d4147b4ecc2f89f2ff433f
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-17 20:33:55 +1100

    Create Direct3DFragmentShader

---
+++ FinalEngine.Rendering.Direct3D/Pipeline/Direct3DFragmentShader.cs
@@ -0,0 +1,53 @@
+﻿namespace FinalEngine.Rendering.Direct3D.Pipeline
+{
+    using System;
+    using FinalEngine.Rendering.Pipeline;
+    using Vortice.Direct3D11;
+
+    public sealed class Direct3DFragmentShader : IShader
+    {
+        private bool isDisposed;
+
+        private ID3D11PixelShader resource;
+
+        public Direct3DFragmentShader(ID3D11PixelShader resource)
+        {
+            this.resource = resource ?? throw new ArgumentNullException(nameof(resource), $"The specified { nameof(resource) } parameter is null.");
+        }
+
+        ~Direct3DFragmentShader()
+        {
+            Dispose(false);
+        }
+
+        public PipelineTarget EntryPoint
+        {
+            get { return PipelineTarget.Fragment; }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                if (resource != null)
+                {
+                    resource.Release();
+                    resource = null;
+                }
+            }
+
+            isDisposed = true;
+        }
+    }
+}
