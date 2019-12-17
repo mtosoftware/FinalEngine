be06131b1e86be6ac5f544828cbf1419f6be668a
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 00:52:22 +1100

    Documentation

--- FinalEngine.Rendering/Buffers/BufferType.cs
+++ FinalEngine.Rendering/Buffers/BufferType.cs
@@ -1,9 +1,18 @@
 ﻿namespace FinalEngine.Rendering.Buffers
 {
+    /// <summary>
+    ///   Enumerates the available buffer types.
+    /// </summary>
     public enum BufferType
     {
+        /// <summary>
+        ///   Specifies the buffer stores vertex data.
+        /// </summary>
         VertexBuffer,
 
+        /// <summary>
+        ///   Specifies the buffer stores 32-bit unsigned integer data.
+        /// </summary>
         IndexBuffer
     }
 }
--- FinalEngine.Rendering/IInputAssembler.cs
+++ FinalEngine.Rendering/IInputAssembler.cs
@@ -2,10 +2,25 @@
 {
     using FinalEngine.Rendering.Buffers;
 
+    /// <summary>
+    ///   Defines an interface that represents the input assembly stage of a rendering pipeline.
+    /// </summary>
     public interface IInputAssembler
     {
+        /// <summary>
+        ///   Binds the specified <paramref name="buffer"/> to the GPU, preparing it for use with subsequent draw commands.
+        /// </summary>
+        /// <param name="buffer">
+        ///   Specifies a <see cref="IBuffer"/> that represents the buffer to bind to the GPU.
+        /// </param>
         void SetBuffer(IBuffer buffer);
 
+        /// <summary>
+        ///   Sets the primitive type and data order that describes the input data to assembled.
+        /// </summary>
+        /// <param name="topology">
+        ///   Specifies a <see cref="PrimitiveTopology"/> that represents the topology.
+        /// </param>
         void SetPrimitiveTopology(PrimitiveTopology topology);
     }
 }
