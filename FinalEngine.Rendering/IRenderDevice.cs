7b1f1120911d1c89c47e33a4eef47877a568db78
Mathew O'Dwyer <mathew.odwyer@outlookl.com>
2019-12-18 02:00:46 +1100

    Create IRenderDevice

---
+++ FinalEngine.Rendering/IRenderDevice.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalEngine.Rendering
+{
+    public interface IRenderDevice
+    {
+        IGPUResourceFactory Factory { get; }
+
+        IInputAssembler InputAssembler { get; }
+
+        ISwapChain SwapChain { get; }
+
+        void Clear(float r, float g, float b, float a);
+
+        void DrawIndices(int first, int count);
+    }
+}
