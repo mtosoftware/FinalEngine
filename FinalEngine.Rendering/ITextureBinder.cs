783d3b85f41bce41ca99310b1d59fb1095e18484
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-04-25 00:40:45 +1000

    Create TextureBinder

---
+++ FinalEngine.Rendering/ITextureBinder.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ITextureBinder.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using FinalEngine.Rendering.Textures;
+
+    public interface ITextureBinder
+    {
+        bool ShouldReset { get; }
+
+        int GetTextureSlotIndex(ITexture texture);
+
+        void Reset();
+    }
+}
---
+++ FinalEngine.Rendering/TextureBinder.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="TextureBinder.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using System;
+    using System.Collections.Generic;
+    using FinalEngine.Rendering.Textures;
+
+    public class TextureBinder : ITextureBinder
+    {
+        private readonly IPipeline pipeline;
+
+        private readonly IDictionary<ITexture, int> textureToSlotMap;
+
+        public TextureBinder(IPipeline pipeline)
+        {
+            this.pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline), $"The specified {nameof(pipeline)} parameter cannot be null.");
+
+            this.textureToSlotMap = new Dictionary<ITexture, int>();
+
+            for (int i = 0; i < pipeline.MaxTextureSlots; i++)
+            {
+                pipeline.SetUniform($"u_textures[{i}]", i);
+            }
+        }
+
+        public bool ShouldReset
+        {
+            get { return this.textureToSlotMap.Count >= this.pipeline.MaxTextureSlots; }
+        }
+
+        public int GetTextureSlotIndex(ITexture texture)
+        {
+            if (texture == null)
+            {
+                throw new ArgumentNullException(nameof(texture), $"The specified {nameof(texture)} parameter cannot be null.");
+            }
+
+            if (this.textureToSlotMap.ContainsKey(texture))
+            {
+                return this.textureToSlotMap[texture];
+            }
+
+            int slot = this.textureToSlotMap.Count;
+
+            this.pipeline.SetTexture(texture, slot);
+            this.textureToSlotMap.Add(texture, slot);
+
+            return slot;
+        }
+
+        public void Reset()
+        {
+            this.textureToSlotMap.Clear();
+        }
+    }
+}
