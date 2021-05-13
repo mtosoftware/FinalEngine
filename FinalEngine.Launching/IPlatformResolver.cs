0c7bf41106a300999ec765b20c2b553f6616923c
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-05-13 21:00:56 +1000

    Create IPlatformResolver

---
+++ FinalEngine.Launching/IPlatformResolver.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IPlatformResolver.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Launching
+{
+    using System.Runtime.InteropServices;
+
+    public interface IPlatformResolver
+    {
+        void Register<TFactory>(OSPlatform platform, bool remove = false)
+            where TFactory : IGamePlatformFactory, new();
+
+        IGamePlatformFactory Resolve();
+    }
+}
