0d9883883f7285eb800f3904a11b566273ebf346
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-05-13 20:59:41 +1000

    Create IGamePlatformFactory and GamePlatformSettings

---
+++ FinalEngine.Launching/GamePlatformSettings.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="GamePlatformSettings.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Launching
+{
+    public struct GamePlatformSettings
+    {
+        public int WindowHeight { get; init; }
+
+        public string WindowTitle { get; init; }
+
+        public int WindowWidth { get; init; }
+    }
+}
---
+++ FinalEngine.Launching/IGamePlatformFactory.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="IGamePlatformFactory.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Launching
+{
+    using FinalEngine.Input.Keyboard;
+    using FinalEngine.Input.Mouse;
+    using FinalEngine.IO;
+    using FinalEngine.Platform;
+    using FinalEngine.Rendering;
+    using FinalEngine.Rendering.Textures;
+
+    public interface IGamePlatformFactory
+    {
+        void InitializePlatform(
+            GamePlatformSettings settings,
+            out IWindow window,
+            out IEventsProcessor eventsProcessor,
+            out IFileSystem fileSystem,
+            out IKeyboard keyboard,
+            out IMouse mouse,
+            out IRenderContext renderContext,
+            out IRenderDevice renderDevice,
+            out ITexture2DLoader textureLoader);
+    }
+}
