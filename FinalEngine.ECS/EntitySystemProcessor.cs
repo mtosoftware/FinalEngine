17fa8b79d9016e9bdf4086aa6c286771f79c90f8
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-06-07 18:29:09 +1000

    Create IEntitySystemsProcessor

---
+++ FinalEngine.ECS/EntitySystemProcessor.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IEntitySystemsProcessor.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS
+{
+    public interface IEntitySystemsProcessor
+    {
+        void ProcessAll(GameLoopType type);
+    }
+}
