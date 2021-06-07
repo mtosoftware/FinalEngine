36cdfaec224b864171278d074fcf56b67233cf4f
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-06-07 18:25:36 +1000

    Create IEntityFactory

---
+++ FinalEngine.ECS/IEntityFactory.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IEntityFactory.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS
+{
+    public interface IEntityFactory
+    {
+        Entity CreateEntity();
+    }
+}
