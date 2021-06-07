0e8883613ad7b623444aa92cb63f8f5ab4a0dee1
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-06-07 19:19:32 +1000

    Going to start writing up unit tests now

--- TestGame/Game.cs
+++ TestGame/Game.cs
@@ -5,57 +5,24 @@
 namespace TestGame
 {
     using System.Drawing;
-    using System.Numerics;
-    using FinalEngine.ECS;
     using FinalEngine.Input.Keyboard;
     using FinalEngine.Launching;
     using FinalEngine.Rendering;
-    using FinalEngine.Rendering.Textures;
-    using TestGame.Temp;
 
     public class Game : GameContainer
     {
         private readonly ISpriteDrawer drawer;
-
-        private readonly ITexture2D textureA;
-
-        private readonly EntityWorld world;
 
         public Game()
         {
             var binder = new TextureBinder(this.RenderDevice.Pipeline);
             var batcher = new SpriteBatcher(this.RenderDevice.InputAssembler);
             this.drawer = new SpriteDrawer(this.RenderDevice, batcher, binder, this.Window!.ClientSize.Width, this.Window!.ClientSize.Height);
-
-            this.textureA = this.TextureLoader.LoadTexture("cheese.jpg");
-
-            this.world = new EntityWorld(
-                new SpriteRenderSystem(this.drawer));
-
-            var entity = new Entity();
-
-            entity.AddComponent(new TransformComponent()
-            {
-                Position = Vector2.Zero,
-                Rotation = 0,
-                Scale = new Vector2(256, 256),
-            });
-
-            entity.AddComponent(new Sprite()
-            {
-                Color = Color.Red,
-                Origin = new Vector2(128, 128),
-                Texture = this.textureA,
-            });
-
-            this.world.AddEntity(entity);
         }
 
         protected override void Render()
         {
             this.RenderDevice.Clear(Color.CornflowerBlue);
-
-            this.world.ProcessAll(GameLoopType.Render);
 
             base.Render();
         }
@@ -66,8 +33,6 @@ namespace TestGame
             {
                 this.Exit();
             }
-
-            this.world.ProcessAll(GameLoopType.Update);
 
             base.Update();
         }
