ca879f61fc99d2970e8c17ef0425c416a5429b05
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-06-07 17:41:05 +1000

    Create IComponent

---
+++ FinalEngine.ECS/IComponent.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="IComponent.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS
+{
+    public interface IComponent
+    {
+    }
+}
