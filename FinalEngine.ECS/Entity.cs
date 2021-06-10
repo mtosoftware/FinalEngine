1c796b17c95454d3381474bfb4b54267ed21dd22
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2021-06-10 20:56:19 +1000

    Add documentation to Entity

--- FinalEngine.ECS/Entity.cs
+++ FinalEngine.ECS/Entity.cs
@@ -8,17 +8,46 @@ namespace FinalEngine.ECS
     using System.Collections.Generic;
     using System.Dynamic;
 
+    /// <summary>
+    ///   Provides a container for <see cref="IComponent"/> s that can be, added, removed or accessed during runtime through an <see cref="EntitySystemBase"/>.
+    /// </summary>
+    /// <seealso cref="System.Dynamic.DynamicObject"/>
+    /// <seealso cref="FinalEngine.ECS.IReadOnlyEntity"/>
     public class Entity : DynamicObject, IReadOnlyEntity
     {
-        internal EventHandler<EventArgs>? OnComponentsChanged;
-
+        /// <summary>
+        ///   Represents a type to <see cref="IComponent"/> map.
+        /// </summary>
         private readonly IDictionary<Type, IComponent> typeToComponentMap;
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="Entity"/> class.
+        /// </summary>
         public Entity()
         {
             this.typeToComponentMap = new Dictionary<Type, IComponent>();
         }
 
+        /// <summary>
+        ///   Gets the event that occurs when a component is added or removed from this <see cref="Entity"/>.
+        /// </summary>
+        /// <value>
+        ///   The event that occurs when a component is added or removed from this <see cref="Entity"/>.
+        /// </value>
+        internal EventHandler<EventArgs>? OnComponentsChanged { get; }
+
+        /// <summary>
+        ///   Adds the specified <paramref name="component"/> to this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="component">
+        ///   The component to add to this <see cref="Entity"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="component"/> parameter cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   The specified <paramref name="component"/> parameters type has already been added to this entity.
+        /// </exception>
         public void AddComponent(IComponent component)
         {
             if (component == null)
@@ -37,12 +66,30 @@ namespace FinalEngine.ECS
             this.OnComponentsChanged?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        ///   Instantiates and adds the specified <typeparamref name="TComponent"/> to this <see cref="Entity"/>.
+        /// </summary>
+        /// <typeparam name="TComponent">
+        ///   The type component to add to this <see cref="Entity"/>.
+        /// </typeparam>
         public void AddComponent<TComponent>()
             where TComponent : IComponent, new()
         {
             this.AddComponent(Activator.CreateInstance<TComponent>());
         }
 
+        /// <summary>
+        ///   Determines whether the specified <paramref name="component"/> is contained within this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="component">
+        ///   The component to check.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <paramref name="component"/> is contained within this <see cref="Entity"/>; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="component"/> parameter cannot be null.
+        /// </exception>
         public bool ContainsComponent(IComponent component)
         {
             if (component == null)
@@ -61,6 +108,21 @@ namespace FinalEngine.ECS
             return false;
         }
 
+        /// <summary>
+        ///   Determines whether a component of the specified <paramref name="type"/> is contained within this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="type">
+        ///   The type of component to check.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the a component of the specified <paramref name="type"/> is contained within this <see cref="Entity"/>; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   type - The specified <paramref name="type"/> parameter cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   The specified <paramref name="type"/> parameter does not implement <see cref="IComponent"/>.
+        /// </exception>
         public bool ContainsComponent(Type type)
         {
             if (type == null)
@@ -76,12 +138,36 @@ namespace FinalEngine.ECS
             return this.typeToComponentMap.ContainsKey(type);
         }
 
+        /// <summary>
+        ///   Determines whether a component of the specified <typeparamref name="TComponent"/> is contained within this <see cref="Entity"/>.
+        /// </summary>
+        /// <typeparam name="TComponent">
+        ///   The type of component to check.
+        /// </typeparam>
+        /// <returns>
+        ///   <c>true</c> if a component of the specified <typeparamref name="TComponent"/> is contained within this <see cref="Entity"/>; otherwise, <c>false</c>.
+        /// </returns>
         public bool ContainsComponent<TComponent>()
             where TComponent : IComponent
         {
             return this.ContainsComponent(typeof(TComponent));
         }
 
+        /// <summary>
+        ///   Gets a component of the specified <paramref name="type"/> from this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="type">
+        ///   The type of component to retrieve.
+        /// </param>
+        /// <returns>
+        ///   The component of the specified <paramref name="type"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="type"/> parameter cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   The specified <paramref name="type"/> parameter does not implement <see cref="IComponent"/> or the specified <paramref name="type"/> parameter is not a component type that has been added to this <see cref="Entity"/>.
+        /// </exception>
         public IComponent GetComponent(Type type)
         {
             if (type == null)
@@ -102,12 +188,36 @@ namespace FinalEngine.ECS
             return this.typeToComponentMap[type];
         }
 
+        /// <summary>
+        ///   Gets a component of the specified <typeparamref name="TComponent"/> from this <see cref="Entity"/>.
+        /// </summary>
+        /// <typeparam name="TComponent">
+        ///   The type of component to retrieve.
+        /// </typeparam>
+        /// <returns>
+        ///   The component of the specified <typeparamref name="TComponent"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///   The specified <typeparamref name="TComponent"/> parameter is not a component type that has been added to this <see cref="Entity"/>.
+        /// </exception>
         public TComponent GetComponent<TComponent>()
             where TComponent : IComponent
         {
             return (TComponent)this.GetComponent(typeof(TComponent));
         }
 
+        /// <summary>
+        ///   Removes the specified <paramref name="component"/> from this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="component">
+        ///   The component to remove.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   The specified <paramref name="component"/> parameter cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   The specified <paramref name="component"/> parameter has not been added to this entity.
+        /// </exception>
         public void RemoveComponent(IComponent component)
         {
             if (component == null)
@@ -123,6 +233,18 @@ namespace FinalEngine.ECS
             this.RemoveComponent(component.GetType());
         }
 
+        /// <summary>
+        ///   Removes a component of the specified <paramref name="type"/> from this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="type">
+        ///   The type of component to remove.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   type - The specified <paramref name="type"/> parameter cannot be null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   The specified <paramref name="type"/> parameter does not implement <see cref="IComponent"/> or the specified <paramref name="type"/> parameter is not a component type that has been added to this entity.
+        /// </exception>
         public void RemoveComponent(Type type)
         {
             if (type == null)
@@ -144,12 +266,59 @@ namespace FinalEngine.ECS
             this.OnComponentsChanged?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        ///   Removes a component of the specified <typeparamref name="TComponent"/> from this <see cref="Entity"/>.
+        /// </summary>
+        /// <typeparam name="TComponent">
+        ///   The type of the component to remove.
+        /// </typeparam>
+        /// <exception cref="ArgumentException">
+        ///   A component of the specified <typeparamref name="TComponent"/> parameter has not been added to this entity.
+        /// </exception>
         public void RemoveComponent<TComponent>()
             where TComponent : IComponent
         {
             this.RemoveComponent(typeof(TComponent));
         }
 
+        /// <summary>
+        ///   Attempts to retrieve a component by class name.
+        /// </summary>
+        /// <param name="binder">
+        ///   Provides information about the object that called the dynamic operation. The <c>binder.Name</c> property provides the name of the member on which the dynamic operation is performed. For example, for the <c>Console.WriteLine(sampleObject.SampleProperty)</c> statement, where <c>sampleObject</c> is an instance of the class derived from the <see cref="DynamicObject"/> class, <c>binder.Name</c> returns "SampleProperty". The <c>binder.IgnoreCase</c> property specifies whether the member name is case-sensitive.
+        /// </param>
+        /// <param name="result">
+        ///   The component that has been retrieved; otherwise, <c>null</c>.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the operation is successful; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   binder - The specified <paramref name="binder"/> parameter cannot be null.
+        /// </exception>
+        /// <remarks>
+        ///   If the name of the component you wish to access has a suffix of `Component`, you can choose whether to omit it when retrieving the component. See the example for further explanation.
+        /// </remarks>
+        /// <example>
+        ///   <code>
+        /// // Create an entity and add a component.
+        /// dynamic entity = new Entity();
+        /// var component = new ExampleComponent();
+        ///
+        /// entity.AddComponent(component);
+        ///
+        /// // You can then retrieve the component like so:
+        /// var example = entity.GetComponent(typeof(ExampleComponent));
+        ///
+        /// // Or just the generic version for compile time stuff.
+        /// var example = entity.GetComponent&lt;ExampleComponent&gt;();
+        ///
+        /// // Or even access it dynamically via it's class name.
+        /// ExampleComponent example = entity.ExampleComponent;
+        ///
+        /// // Lastly, you can make it shorter by dropping the suffix.
+        /// ExampleComponent example = entity.Example;</code>
+        /// </example>
         public override bool TryGetMember(GetMemberBinder binder, out object? result)
         {
             if (binder == null)
