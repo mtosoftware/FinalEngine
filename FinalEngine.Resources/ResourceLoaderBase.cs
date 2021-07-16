1c4ebb46f6a8782251943720a8b2596d9c592100
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-16 18:36:26 +1000

    Create ResourceLoaderBase class

---
+++ FinalEngine.Resources/ResourceLoaderBase.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ResourceLoaderBase.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Resources
+{
+    using System;
+
+    public abstract class ResourceLoaderBase<T> : IResourceLoaderInternal
+        where T : IResource
+    {
+        public abstract T LoadResource(string filePath);
+
+        IResource IResourceLoaderInternal.LoadResource(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath), $"The specified {nameof(filePath)} parameter cannot be null.");
+            }
+
+            return this.LoadResource(filePath);
+        }
+    }
+}
