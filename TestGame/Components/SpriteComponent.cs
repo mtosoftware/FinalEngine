115f783d240e698f6e76841b3812502ebae99077
Mathew ODwyer <mathew.odwyer@outlook.com>
2021-07-16 19:00:50 +1000

    Making changes within solution to accomodate the resource manager

---
+++ TestGame/Components/SpriteComponent.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="SpriteComponent.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace TestGame.Components
+{
+    using System.Drawing;
+    using System.Numerics;
+    using FinalEngine.ECS;
+    using FinalEngine.Rendering.Textures;
+
+    public sealed class SpriteComponent : IComponent
+    {
+        public Color Color { get; set; }
+
+        public Vector2 Origin { get; set; }
+
+        public ITexture2D? Texture { get; set; }
+    }
+}
