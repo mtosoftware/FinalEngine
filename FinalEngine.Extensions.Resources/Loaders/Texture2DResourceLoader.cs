3185d12ab069a46e88dd992645f6adf57b07f5b7
mathewodwyer <mathew.odwyer@interfisystems.com>
2022-02-05 12:36:58 +1100

    Updated namespace

--- FinalEngine.Extensions.Resources/Loaders/Texture2DResourceLoader.cs
+++ FinalEngine.Extensions.Resources/Loaders/Texture2DResourceLoader.cs
@@ -2,7 +2,7 @@
 //     Copyright (c) Software Antics. All rights reserved.
 // </copyright>
 
-namespace FinalEngine.Rendering.Textures
+namespace FinalEngine.Extensions.Resources.Loaders
 {
     using System;
     using System.IO;
