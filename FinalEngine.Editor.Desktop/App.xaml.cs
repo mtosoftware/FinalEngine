d8b23ec026245ba4a8be225adb713ec296857a83
Software Antics <50978201+softwareantics@users.noreply.github.com>
2022-02-26 19:16:51 +1100

    FE-5 - Visual Studio like UI elements. (#114)
    * FE-5 - Added visual studio styling to editor.
    
    * FE-5 - Removed failing unit tests

---
+++ FinalEngine.Editor.Desktop/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="App.xaml.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Editor.Desktop
+{
+    using System.Windows;
+    using FinalEngine.Editor.Desktop.Views;
+
+    /// <summary>
+    ///   Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        ///   Raises the <see cref="Application.Startup"/> event.
+        /// </summary>
+        /// <param name="e">
+        ///   A <see cref="StartupEventArgs"/> that contains the event data.
+        /// </param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var view = new MainView();
+            view.Show();
+        }
+    }
+}
