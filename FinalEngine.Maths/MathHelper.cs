a90fb4cb0c15aa9930fe164225a8f385a670d77b
Mathew ODwyer <mathew.odwyer@outlook.com>
2022-10-06 00:57:00 +1100

    Added missing default texture and MathHelper class

---
+++ FinalEngine.Maths/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="MathHelper.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Maths
+{
+    using System;
+
+    public static class MathHelper
+    {
+        public static float DegreesToRadians(float angle)
+        {
+            return (float)Math.PI / 180.0f * angle;
+        }
+
+        public static float RadiansToDegrees(float radians)
+        {
+            return 180.0f / (float)Math.PI * radians;
+        }
+    }
+}
