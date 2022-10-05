e4e610c83f0bf6b8098e7c7c03a684972e3c489e
Mathew ODwyer <mathew.odwyer@outlook.com>
2022-10-06 00:50:55 +1100

    Created Material

---
+++ FinalEngine.Rendering/IMaterial.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IMaterial.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using FinalEngine.Rendering.Textures;
+
+    public interface IMaterial
+    {
+        ITexture2D? DiffuseTexture { get; set; }
+
+        void Bind(IPipeline pipeline);
+    }
+}
---
+++ FinalEngine.Rendering/Material.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Material.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.Rendering
+{
+    using System;
+    using FinalEngine.Rendering.Textures;
+    using FinalEngine.Resources;
+
+    public class Material : IMaterial
+    {
+        private const int DiffuseTextureSlot = 0;
+
+        private static readonly ITexture2D DefaultDiffuseTexture = ResourceManager.Instance.LoadResource<ITexture2D>("Resources\\Textures\\default_diffuse.png");
+
+        public ITexture2D? DiffuseTexture { get; set; }
+
+        public void Bind(IPipeline pipeline)
+        {
+            if (pipeline == null)
+            {
+                throw new ArgumentNullException(nameof(pipeline));
+            }
+
+            if (this.DiffuseTexture == null)
+            {
+                this.DiffuseTexture = DefaultDiffuseTexture;
+            }
+
+            pipeline.SetUniform("u_material.diffuseTexture", DiffuseTextureSlot);
+            pipeline.SetTexture(this.DiffuseTexture, DiffuseTextureSlot);
+        }
+    }
+}
