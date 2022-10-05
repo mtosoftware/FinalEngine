10af6d8303e6b7774657405a1d8a5b22ee603dfb
Mathew ODwyer <mathew.odwyer@outlook.com>
2022-10-06 01:18:09 +1100

    Create MeshComponent

---
+++ FinalEngine.ECS.Components/Rendering/MeshComponent.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="MeshComponent.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS.Components.Rendering
+{
+    using FinalEngine.Rendering;
+
+    public class MeshComponent : IComponent
+    {
+        public IMaterial? Material { get; set; }
+
+        public IMesh? Mesh { get; }
+    }
+}
