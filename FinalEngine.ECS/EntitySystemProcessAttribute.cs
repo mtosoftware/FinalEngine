525769feb5bbe338b69e57b6b1cd5f666e1834b6
Mathew ODwyer <mathew.odwyer@outlook.com>
2022-11-13 22:05:31 +1100

    I wrote tests for this lol no I didn't fuck my life never again.

---
+++ FinalEngine.ECS/EntitySystemProcessAttribute.cs
@@ -0,0 +1,24 @@
+// <copyright file="EntitySystemProcessAttribute.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS
+{
+    using System;
+
+    /// <summary>
+    ///   Indicates the execution type of an entity system.
+    /// </summary>
+    /// <seealso cref="Attribute"/>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class EntitySystemProcessAttribute : Attribute
+    {
+        /// <summary>
+        ///   Gets the execution type.
+        /// </summary>
+        /// <value>
+        ///   The execution type.
+        /// </value>
+        public GameLoopType ExecutionType { get; init; }
+    }
+}
