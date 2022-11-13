bb979460d887c690852cc384c166e58decc36a5c
Mathew ODwyer <mathew.odwyer@outlook.com>
2022-11-13 21:48:19 +1100

    Add GameLoopType

---
+++ FinalEngine.ECS/GameLoopType.cs
@@ -0,0 +1,22 @@
+// <copyright file="GameLoopType.cs" company="Software Antics">
+//     Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+namespace FinalEngine.ECS
+{
+    /// <summary>
+    ///   Enumerates the available execution types for an entity system.
+    /// </summary>
+    public enum GameLoopType
+    {
+        /// <summary>
+        ///   The system will be processed per update.
+        /// </summary>
+        Update,
+
+        /// <summary>
+        ///   The system will be processed per render.
+        /// </summary>
+        Render,
+    }
+}
