c9f6eb228b55a54f7cfcf0551d19c7050df7416a
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-02-08 19:02:13 +1100

    Woot

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.1.498.1440")]
-[assembly: AssemblyFileVersion("2023.1.498.1440")]
+[assembly: AssemblyVersion("2023.1.499.1449")]
+[assembly: AssemblyFileVersion("2023.1.499.1449")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
