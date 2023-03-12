a8664ae8fcc0758dc443f77e20c2d8273d66800f
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-03-12 22:47:45 +1100

    Remove GUI crap

--- FinalEngine.GUI/Panels/Panel.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="Panel.cs" company="Software Antics">
-//     Copyright (c) Software Antics. All rights reserved.
-// </copyright>
-
-namespace FinalEngine.GUI.Panels;
-
-public class Panel : ControlBase
-{
-}
