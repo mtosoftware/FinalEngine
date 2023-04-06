312950f751c668ac376844e42d24f3aa55ca9a21
Mathew ODwyer <mathew.odwyer@outlook.com>
2023-04-07 05:17:37 +1000

    Fixed up styling for base controls so far

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.1.1541.0604")]
-[assembly: AssemblyFileVersion("2023.1.1541.0604")]
+[assembly: AssemblyVersion("2023.2.1541.0604")]
+[assembly: AssemblyFileVersion("2023.2.1541.0604")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
