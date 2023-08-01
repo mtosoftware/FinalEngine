e5e2411eb9e2c0fcf42720beb6e5e320e11337b0
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-01 19:30:57 +1000

    Slowly getting there

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.3.2.0442")]
-[assembly: AssemblyFileVersion("2023.3.2.0442")]
+[assembly: AssemblyVersion("2023.3.22.0442")]
+[assembly: AssemblyFileVersion("2023.3.22.0442")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
