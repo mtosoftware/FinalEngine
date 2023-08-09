099538610c4b4faee4f0036606b05c3d60360df3
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-09 18:28:36 +1000

    Added documentation and unit tests Factory<T>.

--- FinalEngine.Editor.Common/Services/Factories/Factory.cs
+++ FinalEngine.Editor.Common/Services/Factories/Factory.cs
@@ -6,15 +6,35 @@ namespace FinalEngine.Editor.Common.Services.Factories;
 
 using System;
 
+/// <summary>
+/// Provides a standard implementation of an <see cref="IFactory{T}"/>.
+/// </summary>
+/// <typeparam name="T">
+/// The type of instance to crate.
+/// </typeparam>
+/// <seealso cref="IFactory{T}"/>
 public sealed class Factory<T> : IFactory<T>
 {
+    /// <summary>
+    /// The function required to create the instance.
+    /// </summary>
     private readonly Func<T> factory;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Factory{T}"/> class.
+    /// </summary>
+    /// <param name="factory">
+    /// The function used when creating the required instance.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// The specified <paramref name="factory"/> parameter cannot be null.
+    /// </exception>
     public Factory(Func<T> factory)
     {
         this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
     }
 
+    /// <inheritdoc/>
     public T Create()
     {
         return this.factory();
--- FinalEngine.Editor.Common/Services/Factories/IFactory.cs
+++ FinalEngine.Editor.Common/Services/Factories/IFactory.cs
@@ -4,7 +4,25 @@
 
 namespace FinalEngine.Editor.Common.Services.Factories;
 
+using FinalEngine.Editor.Common.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Defines an interface that provides a generic factory method.
+/// </summary>
+/// <typeparam name="T">
+/// The type of instance to create.
+/// </typeparam>
+/// <remarks>
+/// This interface is typically used alongside <see cref="ServiceCollectionExtensions.AddFactory{TService, TImplementation}(IServiceCollection)"/> exteension to provide a factory for view models.
+/// </remarks>
 public interface IFactory<out T>
 {
+    /// <summary>
+    /// Creates an instance of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// The newly created <typeparamref name="T"/> instance.
+    /// </returns>
     T Create();
 }
