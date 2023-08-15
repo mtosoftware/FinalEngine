0f6cafa261a9c2184947beb8cc345d0369d7500f
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-15 20:57:54 +1000

    Added more unit tests

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.3.2398.0")]
-[assembly: AssemblyFileVersion("2023.3.2397.0")]
+[assembly: AssemblyVersion("2023.3.2402.0")]
+[assembly: AssemblyFileVersion("2023.3.2401.0")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
