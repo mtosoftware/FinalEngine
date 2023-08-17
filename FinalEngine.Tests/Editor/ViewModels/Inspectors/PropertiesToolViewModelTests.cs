d86bfc8aaf5a7c4741c8452450d605d87bb1cc03
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-18 00:43:57 +1000

    Fixed issues

--- FinalEngine.Tests/Editor/ViewModels/Inspectors/PropertiesToolViewModelTests.cs
+++ FinalEngine.Tests/Editor/ViewModels/Inspectors/PropertiesToolViewModelTests.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Software Antics. All rights reserved.
 // </copyright>
 
-namespace FinalEngine.Tests.Editor.ViewModels.Docking.Tools.Inspectors;
+namespace FinalEngine.Tests.Editor.ViewModels.Inspectors;
 
 using System;
 using CommunityToolkit.Mvvm.Messaging;
