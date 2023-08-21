15be450f94ee418fad22ee0828bdd24e5e3e01ee
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-21 20:53:31 +1000

    Removed fallbacks, redundant

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.3.3755.0")]
-[assembly: AssemblyFileVersion("2023.3.3753.0")]
+[assembly: AssemblyVersion("2023.3.3766.0")]
+[assembly: AssemblyFileVersion("2023.3.3764.0")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
