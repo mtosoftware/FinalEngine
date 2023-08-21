3abe75f43d7a806aed5d6fd484bcc05187f39f5a
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2023-08-21 14:39:17 +1000

    Remove license stuff

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2023 Software Antics")]
 [assembly: AssemblyTrademark("Software Antics™")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2023.3.3377.0")]
-[assembly: AssemblyFileVersion("2023.3.3375.0")]
+[assembly: AssemblyVersion("2023.3.3387.0")]
+[assembly: AssemblyFileVersion("2023.3.3385.0")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
