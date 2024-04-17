ab6181845bb1230dbdd1a130a0cde1dfa2ae7fe5
Software Antics <mathew.t.odwyer@gmail.com>
2024-04-18 00:51:10 +1000

    Rename Tag to TagComponent

--- FinalEngine.ECS/Components/Core/TagComponent.cs
+++ FinalEngine.ECS/Components/Core/TagComponent.cs
@@ -1,4 +1,4 @@
-// <copyright file="Tag.cs" company="Software Antics">
+// <copyright file="TagComponent.cs" company="Software Antics">
 //     Copyright (c) Software Antics. All rights reserved.
 // </copyright>
 
@@ -17,7 +17,7 @@ using System.ComponentModel;
 /// <seealso cref="IEntityComponent" />
 /// <seealso cref="INotifyPropertyChanged" />
 [Category("Core")]
-public sealed class Tag : IEntityComponent, INotifyPropertyChanged
+public sealed class TagComponent : IEntityComponent, INotifyPropertyChanged
 {
     private string? name;
 
